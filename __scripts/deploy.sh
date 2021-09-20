414d99cf9baab9b2b849a739d21600dda80b78ed
pazams <maor@pazams.com>
2021-09-20 11:33:28 -0600

    Cleanup

--- __scripts/deploy.sh
+++ __scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#See readme for each env vards to have set before running this script
+#See readme for each env vars to have set before running this script
 envsubst < "app-subst.yaml" > "app.yaml"
 
 GO111MODULE=on gcloud app deploy --project=$GCP_PROJECT
